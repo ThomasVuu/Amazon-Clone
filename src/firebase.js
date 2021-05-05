58f89c677594bf881c48f3c67431d2b82cb1908c
Thomas Vu <thomas.vu@hotmail.com>
2021-05-05 21:35:23 +0200

    Home, Header and Product component

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyC3KPOiELvOQOsKj0GajXg1aaMlUiLKmT4",
+    authDomain: "clone-4eeb4.firebaseapp.com",
+    projectId: "clone-4eeb4",
+    storageBucket: "clone-4eeb4.appspot.com",
+    messagingSenderId: "633334886804",
+    appId: "1:633334886804:web:8524c8a368f9788657df81",
+    measurementId: "G-TTD4L5NFT4"
+  };
